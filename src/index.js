179a13d525c4f000d9b138a2ac7dc1ea88ad71e0
Adam Pash <adam.pash@gmail.com>
2018-10-22 17:08:37 -0700

    initial commit; endpoints for playing music

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import * as Spotify from "spotify-control";
+
+const app = express();
+
+const startPlaylistOnStereo = async (playlistName) => {
+  await Spotify["start-device"](process.env.STEREO);
+  const playlistId = await Spotify["get-playlist"](playlistName);
+  await Spotify["set-volume"](40);
+  await Spotify["play"]({ context_uri: `spotify:playlist:${playlistId}` });
+}
+
+const start = () => {
+  app.get("/", function(req, res) {
+    res.send("Hello World");
+  });
+
+  app.get("/start-discover", async (req, res) => {
+    await startPlaylistOnStereo("Discover Weekly")
+    res.sendStatus(200);
+  });
+
+  app.get("/start-radar", async (req, res) => {
+    await startPlaylistOnStereo("Release Radar")
+    res.sendStatus(200);
+  });
+
+  const port = process.env.PORT || 3001;
+  app.listen(process.env.PORT || 3001);
+  console.log("App started on", port);
+};
+
+start();
