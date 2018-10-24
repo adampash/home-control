37a61ea32d556654a570ec718e5a51853af2f8c0
Adam Pash <adam.pash@gmail.com>
2018-10-24 10:19:36 -0700

    feat: start monthly playlist

--- src/index.js
+++ src/index.js
@@ -3,29 +3,35 @@ import * as Spotify from "spotify-control";
 
 const app = express();
 
-const startPlaylistOnStereo = async (playlistName) => {
+const startPlaylistOnStereo = async playlistName => {
   await Spotify["start-device"](process.env.STEREO);
   await new Promise(res => setTimeout(res, 1500));
   const playlistId = await Spotify["get-playlist"](playlistName);
   await Spotify["set-volume"](40);
   await Spotify["play"]({ context_uri: `spotify:playlist:${playlistId}` });
-}
+};
 
 const start = () => {
   app.get("/start-discover", async (req, res) => {
-    await startPlaylistOnStereo("Discover Weekly")
+    await startPlaylistOnStereo("Discover Weekly");
     res.sendStatus(200);
   });
 
   app.get("/start-radar", async (req, res) => {
-    await startPlaylistOnStereo("Release Radar")
+    await startPlaylistOnStereo("Release Radar");
+    res.sendStatus(200);
+  });
+
+  app.get("/start-monthly", async (req, res) => {
+    const monthlyPlaylist = await Spotify["get-monthly-playlist"]();
+    await startPlaylistOnStereo(monthlyPlaylist);
     res.sendStatus(200);
   });
 
   app.get("/save-track", async (req, res) => {
     try {
-    await Spotify["add-to-monthly-playlist"]();
-    res.send("Saved song to your monthly playlist");
+      await Spotify["add-to-monthly-playlist"]();
+      res.send("Saved song to your monthly playlist");
     } catch {
       res.send("Something went wrong saving that song");
     }
